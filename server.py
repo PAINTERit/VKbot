9c98310b266d468fc0a545c62b2011b4a12ff8b6
artem <artemv2001@mail.ru>
2022-11-14 02:11:53 +0300

    Улучшен код.

--- server.py
+++ server.py
@@ -11,11 +11,11 @@ from config import VK_GROUP_ID, VK_TOKEN
 
 class BaseServer(AbstractBaseServer):
     """
-    Используется для создания сессии в ВК и запуска команд.
+    Данный метод будет запускать прослушивание сервера.
     """
 
     _vk = vk_api.VkApi(token=VK_TOKEN)
-    _longpoll = VkLongPoll(_vk)
+    __longpoll = VkLongPoll(_vk)
 
     def start(self, COMMAND_LIST: dict) -> None:
         """
@@ -25,7 +25,7 @@ class BaseServer(AbstractBaseServer):
         """
         self.__commands = COMMAND_LIST
 
-        for event in self._longpoll.listen():
+        for event in self.__longpoll.listen():
             self._command_worker(event)
 
     def _command_worker(self, event: Event) -> None:
@@ -49,132 +49,16 @@ class BaseServer(AbstractBaseServer):
         pass
 
 
-class UtilsServer(BaseServer):
-    """
-    Используется для добавления метода отправки сообщения.
-    """
-
-    def _send_msg(self, user_id: int, message: str, keyboard: VkKeyboard = None) -> None:
-        """
-        Метод для отправки сообщения пользователю.
-        :param user_id: int (id пользователя)
-        :param message: str (сообщение для пользователя)
-        :param keyboard: VkKeyboard
-        :return: None
-        """
-        if keyboard:
-            params = {
-                "user_id": user_id,
-                "message": message,
-                "random_id": random.randint(1, 10000),
-                "keyboard": keyboard.get_keyboard(),
-            }
-        else:
-            params = {
-                "user_id": user_id,
-                "message": message,
-                "random_id": random.randint(1, 10000),
-            }
-        self._vk.method("messages.send", params)
-
-
-class Server(UtilsServer):
-    """
-    Испотзуется для создания методов для бота.
-    """
-
-    def __init__(self):
-        self.keyboard = KeyboardMixin()
-
-    def command_hi(self, event: Event) -> None:
-        """
-        Команда приветсвия.
-        :param event: Event
-        :return: None
-        """
-        self._send_msg(
-            event.user_id,
-            "Привет, я бот, показывающий информацию о погодных условиях!",
-            keyboard=self.keyboard.get_standart_keyboard(),
-        )
-
-    def command_bye(self, event: Event) -> None:
-        """
-        Команда прощания.
-        :param event: Event
-        :return: None
-        """
-        self._send_msg(
-            event.user_id, "Пока, приходи завтра!", keyboard=self.keyboard.get_start()
-        )
-
-    def command_weather(self, event: Event) -> None:
-        """
-        Команда для получения погоды.
-        :param event: Event
-        :return: None
-        """
-        self._send_msg(
-            event.user_id,
-            "Введите страну, город или улицу для получения погоды:",
-            keyboard=self.keyboard.get_weather(),
-        )
-
-    def command_weather_coords(self, event: Event) -> None:
-        """
-        Команда для получения погоды через геолокацию.
-        :param event: Event
-        :return: None
-        """
-        result = self._vk.method(
-            "messages.getById",
-            {"message_ids": [event.message_id], "group_id": VK_GROUP_ID},
-        )
-        geo = result["items"][0]["geo"]["coordinates"]
-        latitude, longitude = geo["latitude"], geo["longitude"]
-        self._send_msg(
-            event.user_id,
-            weather.get_weather_from_coords(latitude, longitude),
-            keyboard=self.keyboard.get_help(),
-        )
-
-    def command_weather_city(self, event: Event) -> None:
-        """
-        Команда для получения погоды через прописанное сообщение от пользователя.
-        :param event: Event
-        :return: None
-        """
-        if event.text:
-            self._send_msg(
-                event.user_id,
-                weather.get_weather_from_city(event.text),
-                keyboard=self.keyboard.get_help(),
-            )
-        else:
-            self.command_weather_coords(event)
-
-    def command_help(self, event: Event) -> None:
-        """
-        Команда для вызова помощи.
-        :param event: Event
-        :return: None
-        """
-        self._send_msg(
-            event.user_id,
-            "Вот какие команды я могу исполнить:",
-            keyboard=self.keyboard.get_standart_keyboard(),
-        )
-
-
 class KeyboardMixin(VkKeyboard, AbstractKeyboardMixin):
     """
     Используется для создания кнопок в боте.
     """
 
     @staticmethod
-    def get_start() -> VkKeyboard:
+    def keyboard_start() -> VkKeyboard:
         """
         Клавитаутра приветствия.
+        Данная клавиатура появляется при прощании с ботом.
         :return: VkKeyboard
         """
         keyboard = VkKeyboard()
@@ -216,3 +100,126 @@ class KeyboardMixin(VkKeyboard, AbstractKeyboardMixin):
         keyboard = VkKeyboard()
         keyboard.add_button(label="Помощь", color=VkKeyboardColor.PRIMARY)
         return keyboard
+
+
+class UtilsServer(BaseServer):
+    """
+    Используется для добавления методов отправки сообщения и получения координат из геопозиции.
+    """
+
+    def _send_msg(
+        self, user_id: int, message: str, keyboard: VkKeyboard = None
+    ) -> None:
+        """
+        Метод для отправки сообщения пользователю.
+        :param user_id: int (id пользователя)
+        :param message: str (сообщение для пользователя)
+        :param keyboard: VkKeyboard
+        :return: None
+        """
+
+        params = {
+            "user_id": user_id,
+            "message": message,
+            "random_id": random.randint(1, 10000),
+        }
+        if keyboard:
+            params["keyboard"] = keyboard.get_keyboard()
+        self._vk.method("messages.send", params)
+
+    def _get_vk_geo(self, event: Event) -> tuple:
+        """
+        Метод для получения координат из геопозиции пользователя.
+        :param event: Event
+        :return: tuple (широта и долгота)
+        """
+        result = self._vk.method(
+            "messages.getById",
+            {"message_ids": [event.message_id], "group_id": VK_GROUP_ID},
+        )
+        geo = result["items"][0]["geo"]["coordinates"]
+        return geo["latitude"], geo["longitude"]
+
+
+class Server(UtilsServer):
+    """
+    Испотзуется для создания методов для бота.
+    """
+
+    keyboard = KeyboardMixin()
+
+    def command_hi(self, event: Event) -> None:
+        """
+        Команда приветсвия.
+        :param event: Event
+        :return: None
+        """
+        self._send_msg(
+            event.user_id,
+            "Привет, я бот, показывающий информацию о погодных условиях!",
+            keyboard=self.keyboard.get_standart_keyboard()
+        )
+
+    def command_bye(self, event: Event) -> None:
+        """
+        Команда прощания.
+        :param event: Event
+        :return: None
+        """
+        self._send_msg(
+            event.user_id,
+            "Пока, приходи завтра!",
+            keyboard=self.keyboard.keyboard_start()
+        )
+
+    def command_weather(self, event: Event) -> None:
+        """
+        Команда для получения погоды.
+        :param event: Event
+        :return: None
+        """
+        self._send_msg(
+            event.user_id,
+            "Введите страну, город или улицу для получения погоды:",
+            keyboard=self.keyboard.get_weather()
+        )
+
+    def command_weather_coords(self, event: Event) -> None:
+        """
+        Команда для получения погоды через геолокацию.
+        :param event: Event
+        :return: None
+        """
+        self._send_msg(
+            event.user_id,
+            weather.get_weather_from_coords(
+                self._get_vk_geo(event)[0], self._get_vk_geo(event)[1]),
+            keyboard=self.keyboard.get_help()
+        )
+
+    def command_weather_city(self, event: Event) -> None:
+        """
+        Команда для получения погоды через прописанное сообщение от пользователя.
+        :param event: Event
+        :return: None
+        """
+        if event.text:
+            self._send_msg(
+                event.user_id,
+                weather.get_weather_from_city(event.text),
+                keyboard=self.keyboard.get_help()
+            )
+        else:
+            self.command_weather_coords(event)
+
+    def command_help(self, event: Event) -> None:
+        """
+        Команда для вызова помощи.
+        :param event: Event
+        :return: None
+        """
+        self._send_msg(
+            event.user_id,
+            "Вот какие команды я могу исполнить:",
+            keyboard=self.keyboard.get_standart_keyboard()
+        )
--- weather.py
+++ weather.py
@@ -16,7 +16,7 @@ def get_weather_from_coords(latitude: float, longitude: float) -> str:
     """
     data = requests.get(
         f"https://api.weather.yandex.ru/v2/forecast?lat={latitude}&lon={longitude}&limit=1&hours=false&extra=true",
-        headers=headers
+        headers=headers,
     )
     return get_weather_data(data)
 
@@ -31,14 +31,14 @@ def get_weather_from_city(city: str) -> str:
     location = geolocator.geocode(city)
     data = requests.get(
         f"https://api.weather.yandex.ru/v2/forecast?lat={location.latitude}&lon={location.longitude}&limit=1&hours=false&extra=true",
-        headers=headers
+        headers=headers,
     )
     return get_weather_data(data)
 
 
 def get_city_from_coords(latitude: float, longitude: float) -> str:
     """
-    Запрос для преобразования координат в адрес.
+    API Запрос для преобразования координат в адрес.
     :param latitude: float (широта)
     :param longitude: float (долгота)
     :return: str (адрес)
@@ -47,8 +47,8 @@ def get_city_from_coords(latitude: float, longitude: float) -> str:
         f"https://geocode-maps.yandex.ru/1.x/?format=json&apikey={GEOCODER_TOKEN}&geocode={longitude},{latitude}"
     ).json()
     return city_data["response"]["GeoObjectCollection"]["featureMember"][0][
-        "GeoObject"]["metaDataProperty"]["GeocoderMetaData"][
-        "Address"]["Components"][2]["name"]
+        "GeoObject"
+    ]["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][2]["name"]
 
 
 def get_weather_data(request: json) -> str:
@@ -68,10 +68,8 @@ def get_weather_data(request: json) -> str:
     pressure = data["fact"]["pressure_mm"]
     humidity = data["fact"]["humidity"]
     prec_t = weather_dict.prec_type[data["fact"]["prec_type"]]
-    if prec_t == "дождь":
+    if prec_t == "дождь" or prec_t == "снег":
         prec_s = weather_dict.prec_rain_strength[data["fact"]["prec_strength"]]
-    elif prec_t == "снег":
-        prec_s = weather_dict.prec_snow_strength[data["fact"]["prec_strength"]]
     else:
         prec_s = "без осадков"
     cloud = weather_dict.cloudness[data["fact"]["cloudness"]]
